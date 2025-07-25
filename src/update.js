fe1c412aafac628d9aa3932d511e8d2baa1512d0
Tquilla. <141149698+msalmanrafadhlih@users.noreply.github.com>
2025-07-25 15:31:04 +0700

    Update update.js

--- src/update.js
+++ src/update.js
@@ -114,8 +114,6 @@ I'm a Growing Front-End Developer from Indonesia. I love a challenge and I'm ski
 <br><img align="left" src="https://user-images.githubusercontent.com/65187002/144930161-2f783401-8d27-4fdf-a2f7-cc0ba32f1f1f.gif" width="30%" style="display:inline;"><img align="right" src="https://user-images.githubusercontent.com/65187002/144930161-2f783401-8d27-4fdf-a2f7-cc0ba32f1f1f.gif" width="30%" style="display:inline;">
 
 <!--   GitHub Trophy https://github.com/ryo-ma/github-profile-trophy?tab=readme-ov-file#apply-theme -->
-<!-- Last updated on ${date.toString()} ;-;-->
-<p align="center">${special[2]} Last updated on ${dd}${getDateSuffix(dd)} ${date.toLocaleString('default', { month: 'long' })} ${date.getFullYear()} ${special[2]} ${mm === 11 && dd === 28 ? "<br>and... today is my birthday" : ""}</p>
 <p align="center">
     <a href="https://github.com/ryo-ma/github-profile-trophy"><img width="80" src="https://github-profile-trophy.vercel.app/?username=msalmanrafadhlih&rank=-B,-C,-?&column=1&margin-w=1&no-frame=true&margin-h=15&no-bg=true&theme=dracula&title=Commits"/></a>
     <a href=""><img width="80" src="https://github-profile-trophy.vercel.app/?username=msalmanrafadhlih&rank=-B,-C,-?&column=1&margin-w=1&no-frame=true&margin-h=15&no-bg=true&theme=dracula&title=PullRequest"/></a>
@@ -127,8 +125,8 @@ I'm a Growing Front-End Developer from Indonesia. I love a challenge and I'm ski
     <a href=""><img width="80" src="https://github-profile-trophy.vercel.app/?username=msalmanrafadhlih&rank=-B,-C,-?&column=1&margin-w=1&no-frame=true&margin-h=15&no-bg=true&theme=dracula&title=Issues"/></a>
     <a href=""><img width="335" src="https://github-profile-trophy.vercel.app/?username=msalmanrafadhlih&title=-Commits,-Repositories,-Reviews,-Stars,-Folowers,-Experiences,-Issues,-PullRequest&column=4&margin-w=1&no-frame=true&margin-h=15&no-bg=true&theme=dracula&"/></a>
 </p>
-  <br>
-  <br>
+<!-- Last updated on ${date.toString()} ;-;-->
+<p align="center">${special[2]} Last updated on ${dd}${getDateSuffix(dd)} ${date.toLocaleString('default', { month: 'long' })} ${date.getFullYear()} ${special[2]} ${mm === 11 && dd === 28 ? "<br>and... today is my birthday" : ""}</p>
 
 ##
 
